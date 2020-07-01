87e5b8b2ff0129c1176e8bba043838f0d566d6ad
Alastiel <feanoriris@gmail.com>
2020-07-01 17:03:44 +0300

    Story 06: add react-router-dom

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './News.module.css'
+
+const News = () => {
+    return (
+        <div className={s.hot}>
+            <p>
+                News HOT: little pepega run in home!!!
+            </p>
+        </div>
+    );
+}
+
+export default News;
