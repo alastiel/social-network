0601b9c0611e8786cbdb65a5c5a45ec51ff307a8
Alastiel <feanoriris@gmail.com>
2020-07-20 20:42:50 +0300

    Story 19: Redux

---
+++ src/Redux/redux-store.js
@@ -0,0 +1,13 @@
+﻿import {combineReducers, createStore} from "redux";
+import dialogReduser from "./DialogReduser";
+import profileReduser from "./ProfileReduser";
+
+let redusers = combineReducers({
+    profilePage: profileReduser,
+    messagePage: dialogReduser,
+    
+});
+
+let store = createStore(redusers);
+
+export default store;
