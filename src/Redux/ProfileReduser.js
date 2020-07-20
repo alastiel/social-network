2030793c6ae50926a1406ffca3897e3a53cd08b3
Alastiel <feanoriris@gmail.com>
2020-07-20 20:40:54 +0300

    Story 19: add Redux

--- src/Redux/ProfileReduser.js
+++ src/Redux/ProfileReduser.js
@@ -1,7 +1,16 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReduser = (state, action) => {
+let initialState = {
+    myPostData: [
+        {id: 1, message: 'Hello it is my Network', likesKounts: 0},
+        {id: 2, message: 'I love Java Script and react', likesKounts: 12},
+        {id: 3, message: 'It is my first post', likesKounts: 10}
+    ],
+    newPostText: ''
+}
+
+const profileReduser = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,63 +1,48 @@
-import dialogReduser from "./DialogReduser";
-import profileReduser from "./ProfileReduser";
-
-const ADD_POST = 'ADD-POST';
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const ADD_MESSAGE = 'ADD-MESSAGE';
-const ADD_UPDATE_NEW_MESSAGE_TEXT_ACTION_CREATOR = 'UPDATE-NEW-MESSAGE-TEXT';
-
-
-let store = {
-    _callSubscriber() {
-    },
-    _state: {
-        profilePage: {
-            myPostData: [
-                {id: 1, message: 'Hello it is my Network', likesKounts: 0},
-                {id: 2, message: 'I love Java Script and react', likesKounts: 12},
-                {id: 3, message: 'It is my first post', likesKounts: 10}
-            ],
-            newPostText: ''
-        },
-        messagePage: {
-            dialogs: [
-                {id: 1, name: 'Anya'},
-                {id: 2, name: 'Dima'},
-                {id: 3, name: 'Nastya'},
-                {id: 4, name: 'Oleg'},
-                {id: 5, name: 'Egor'},
-            ],
-            messages: [
-                {id: 1, messages: 'Hello'},
-                {id: 2, messages: 'How are yuo'},
-                {id: 3, messages: 'Pepega is coming'},
-                {id: 4, messages: 'Turn around, he is behind you'},
-            ],
-            newMessageText: ''
-        }
-    },
-    subscribe(observer) {
-        this._callSubscriber = observer;
-    },
-    getState() {
-        return this._state;
-    },
-
-    dispatch(action) {
-        this._state.messagePage = dialogReduser(this._state.messagePage, action);
-        this._state.profilePage = profileReduser(this._state.profilePage, action);
-
-        this._callSubscriber(this._state);
-    }
-}
-
-
-export const addUpdateNewMessageTextActionCreator = (text) =>
-    ({type: ADD_UPDATE_NEW_MESSAGE_TEXT_ACTION_CREATOR, newText:text})
-export const addMessageActionCreator = () => ({type: ADD_MESSAGE})
-
-
-export const addPostActionCreator = () => ({type: ADD_POST})
-export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText:text})
-
-export default store;
+// import dialogReduser from "./DialogReduser";
+// import profileReduser from "./ProfileReduser";
+//
+// let store = {
+//     _callSubscriber() {
+//     },
+//     _state: {
+//         profilePage: {
+//             myPostData: [
+//                 {id: 1, message: 'Hello it is my Network', likesKounts: 0},
+//                 {id: 2, message: 'I love Java Script and react', likesKounts: 12},
+//                 {id: 3, message: 'It is my first post', likesKounts: 10}
+//             ],
+//             newPostText: ''
+//         },
+//         messagePage: {
+//             dialogs: [
+//                 {id: 1, name: 'Anya'},
+//                 {id: 2, name: 'Dima'},
+//                 {id: 3, name: 'Nastya'},
+//                 {id: 4, name: 'Oleg'},
+//                 {id: 5, name: 'Egor'},
+//             ],
+//             messages: [
+//                 {id: 1, messages: 'Hello'},
+//                 {id: 2, messages: 'How are yuo'},
+//                 {id: 3, messages: 'Pepega is coming'},
+//                 {id: 4, messages: 'Turn around, he is behind you'},
+//             ],
+//             newMessageText: ''
+//         }
+//     },
+//     subscribe(observer) {
+//         this._callSubscriber = observer;
+//     },
+//     getState() {
+//         return this._state;
+//     },
+//
+//     dispatch(action) {
+//         this._state.messagePage = dialogReduser(this._state.messagePage, action);
+//         this._state.profilePage = profileReduser(this._state.profilePage, action);
+//
+//         this._callSubscriber(this._state);
+//     }
+// }
+//
+// export default store;
